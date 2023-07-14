cb8f9fe6c8c52e0d259e773958711df8052d48bf
ubeshkrishnanNAME <ubeshkrishnan126@gmail.com>
2023-07-14 13:47:39 +0530

    dashboard api call

--- app/src/screens/Dashboard/DashboardGraph.js
+++ app/src/screens/Dashboard/DashboardGraph.js
@@ -1,122 +1,114 @@
-import { View, Text, StatusBar, StyleSheet, Image } from 'react-native';
 import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, Image, Dimensions } from 'react-native';
 import axios from 'axios';
+import { Url } from "../../../Global_Variable/api_link";
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
 const DashboardGraph = () => {
   const [completed, setCompleted] = useState(0);
   const [incomplete, setIncomplete] = useState(0);
-  const [scheduled, setScheduled] = useState(0);
-  const [unscheduled, setUnscheduled] = useState(0);
+  const [schedule, setSchedule] = useState(0);
+  const [unschedule, setUnschedule] = useState(0);
 
   useEffect(() => {
-    // fetchData();
+    fetchData();
   }, []);
 
   const fetchData = async () => {
     try {
-      const response = await axios.get("http://localhost:5005/dashboard");
-      const data = response.data;
+      const completeResponse = await axios.get(Url + "/complete");
+      const incompleteResponse = await axios.get(Url + "/incomplete");
+      const scheduleResponse = await axios.get(Url + "/schedule");
+      const unscheduleResponse = await axios.get(Url + "/unschedule");
 
-      setCompleted(data.completed);
-      setIncomplete(data.incomplete);
-      setScheduled(data.scheduled);
-      setUnscheduled(data.unscheduled);
+      setCompleted(completeResponse.data[0].completed);
+      setIncomplete(incompleteResponse.data[0].incompleted);
+      setSchedule(scheduleResponse.data[0].scheduled);
+      setUnschedule(unscheduleResponse.data[0].unscheduled);
+      
     } catch (error) {
-      console.log(error);
+      console.log("Error:", error);
+      console.log(scheduleResponse.data);
     }
-  };
-
-  const maxDataValue = Math.max(completed, incomplete, scheduled, unscheduled);
-
-  const calculateBarHeight = (dataValue) => {
-    return (dataValue / maxDataValue) * 150; // Adjust the height scaling factor as needed
   };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.dashboardText}>Dashboard</Text>
       <View style={styles.hrLine} />
-      <View style={styles.graphContainer}>
-        {/* <Image
-          source={require('../../assets/bar.png')}
-          style={[styles.bar, { height: calculateBarHeight(maxDataValue) }]}
-        /> */}
-      </View>
-      <View style={styles.dataContainer}>
-        <View style={styles.rowContainer}>
-          <Text style={styles.dataText}>Completed: {completed}</Text>
-          <Text style={styles.dataText}>Incomplete: {incomplete}</Text>
+      <View style={styles.cardContainer}>
+        <View style={styles.card}>
+          <Text style={styles.cardTitle}>Completed</Text>
+          <Text style={styles.cardData}>{completed}</Text>
         </View>
-        <View style={styles.rowContainer}>
-          <Text style={styles.dataText}>Scheduled: {scheduled}</Text>
-          <Text style={styles.dataText}>Unscheduled: {unscheduled}</Text>
+        <View style={styles.card}>
+          <Text style={styles.cardTitle}>Incomplete</Text>
+          <Text style={styles.cardData}>{incomplete}</Text>
+        </View>
+      </View>
+      <View style={styles.cardContainer}>
+        <View style={styles.card}>
+          <Text style={styles.cardTitle}>Schedule</Text>
+          <Text style={styles.cardData}>{schedule}</Text>
+        </View>
+        <View style={styles.card}>
+          <Text style={styles.cardTitle}>Unschedule</Text>
+          <Text style={styles.cardData}>{unschedule}</Text>
         </View>
       </View>
     </View>
   );
 };
 
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white',
-    padding: 20, // Increase padding to increase the size of the dashboard
+    padding: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   dashboardText: {
     fontSize: 24,
     fontWeight: 'bold',
     color: 'black',
+    textAlign: 'center',
+    // marginBottom: 20,
   },
   hrLine: {
     borderBottomColor: 'black',
     borderBottomWidth: 1,
+    marginVertical: 20,
+    width: '100%',
+  },
+  cardContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    width: '100%',
+  },
+  card: {
+    width: windowWidth * 0.4,
+    height: windowHeight * 0.2,
+    backgroundColor: '#BFDDF3',
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
     marginVertical: 10,
   },
-  graphContainer: {
-    flexDirection: 'row',
-    // justifyContent: 'space-between',
-    marginBottom: 20,
-    // backgroundColor: '#D0E3F1',
-  },
-  bar: {
-    flex: 1,
-    resizeMode: 'contain',
-    height:200,
-    marginTop:40,
-    width:200,
-    backgroundColor:'grey',
-  },
-  dataContainer: {
+  cardTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'black',
     marginBottom: 10,
   },
-  rowContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginBottom: 5,
-  },
-  dataText: {
-    fontSize: 18,
+  cardData: {
+    fontSize: 24,
+    fontWeight: 'bold',
     color: 'black',
-    marginVertical: 5,
-    backgroundColor: '#BFDDF3',
-    width: 150,
-    height: 30,
-    marginTop:70,
-    borderRadius:20,
-    paddingRight:10,
   },
-  dataTextt:{
-    fontSize: 18,
-    color: 'black',
-    marginVertical: 5,
-    backgroundColor: '#BFDDF3',
-    width: 150,
-    height: 30,
-    marginTop:50,
-    borderRadius:20,
-    marginRight:10,
-  }
 });
 
 export default DashboardGraph;
